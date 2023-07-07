cb0912c439b825e0209bd9662958b35bd6b8c0ea
Ilir <ilir.perolli@tenton.co>
2023-07-07 22:23:24 +0200

    feat: added openAi api

---
+++ OpenAi.py
@@ -0,0 +1,32 @@
+import os
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+class OpenAiModel:
+    system_message = None
+    prompt = ""
+    type = ""
+
+    def __init__(self, type, prompt):
+        self.type = type
+        self.prompt = prompt
+
+        self.setTypes()
+
+    def setTypes(self):
+        if self.type == 'deadline':
+            self.system_message = "By the text provided, you need to get the deadline of the application. I need an " \
+                                  "answer provided by you specifying only the date, not followed by a description. " \
+                                  "ONLY THE DATE in format: %d/%m/%Y. If not found, set date 1999/01/01"
+
+    def getResponse(self):
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": self.system_message},
+                {"role": "user", "content": self.prompt}
+            ]
+        )
+        return completion.choices[0].message.content
