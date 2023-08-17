93b51f075a158f574e322f5bc3ad8a7e6a71a390
Ilir <ilir.perolli@tenton.co>
2023-08-17 19:17:02 +0200

    fix: refactored code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
-import threading
+import concurrent.futures
 
+# Assuming you have these imports at the top of your file
 from Jobs.Indeed import Indeed
 from Tenders.Caritas import Caritas
 from Jobs.Gjirafa import Gjirafa
@@ -16,91 +17,44 @@ from Tenders.WorldBank import WorldBank
 
 tender_url = "http://e-tenderi.test/api/tenders"
 job_url = "http://e-tenderi.test/api/jobs"
-
-clean_file = open("logs.txt", "w")
-clean_file.close()
+logs = []
 
 
-def process_data(type, data):
-    for obj in data:
-        try:
-            if type == 'job':
-                response = requests.post(job_url, json=obj)
-            else:
-                response = requests.post(tender_url, json=obj)
+def process_data(type, source):
+    with requests.Session() as session:
+        data = source.get_data()
+        for obj in data:
+            try:
+                url = job_url if type == 'job' else tender_url
+                response = session.post(url, json=obj)
 
-            if response.status_code != 201 and response.status_code != 400:
-                file = open("logs.txt", "a")
-                file.write(f"{obj} \n")
-                file.close()
-
-            print("Response:", response.json(), obj)
-        except requests.exceptions.RequestException as e:
-
-            print("An error occurred:", e, obj)
+                if response.status_code not in [201, 400]:
+                    logs.append((obj, response.json()))
+                print("Response:", response.json(), obj)
+            except requests.exceptions.RequestException as e:
+                print("An error occurred:", e, obj)
 
 
-caritas = Caritas()
-kcsFoundation = KcsFoundation()
-kastori = Kastori()
-kastoriTender = KastoriTender()
-kosovaJob = KosovaJob()
-telegrafi = Telegrafi()
-gjirafa = Gjirafa()
-osce = Osce()
-world_bank = WorldBank()
-undp = Undp()
-cdf = Cdf()
-indeed = Indeed()
+sources = [
+    {'type': 'tender', 'source': Caritas()},
+    {'type': 'tender', 'source': KcsFoundation()},
+    {'type': 'job', 'source': Kastori()},
+    {'type': 'tender', 'source': KastoriTender()},
+    {'type': 'job', 'source': Telegrafi()},
+    {'type': 'job', 'source': Gjirafa()},
+    {'type': 'tender', 'source': Osce()},
+    {'type': 'tender', 'source': WorldBank()},
+    {'type': 'tender', 'source': Undp()},
+    {'type': 'tender', 'source': Cdf()},
+    {'type': 'job', 'source': KosovaJob()},
+    {'type': 'job', 'source': Indeed()}
+]
 
-data_caritas = caritas.get_data()
-data_kcs = kcsFoundation.get_data()
-data_kastori = kastori.get_data()
-data_kastori_tender = kastoriTender.get_data()
-data_kosova_job = kosovaJob.get_data()
-data_telegrafi = telegrafi.get_data()
-data_gjirafa = gjirafa.get_data()
-data_osce = osce.get_data()
-data_world_bank = world_bank.get_data()
-data_undp = undp.get_data()
-data_cdf = cdf.get_data()
-data_indeed = indeed.get_data()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_data, src['type'], src['source']) for src in sources]
+    for future in concurrent.futures.as_completed(futures):
+        future.result()
 
-thread_caritas = threading.Thread(target=process_data, args=('tender', data_caritas,))
-thread_kcs = threading.Thread(target=process_data, args=('tender', data_kcs,))
-thread_kastori = threading.Thread(target=process_data, args=('job', data_kastori,))
-thread_kastori_tender = threading.Thread(target=process_data, args=('tender', data_kastori_tender,))
-thread_kosova_job = threading.Thread(target=process_data, args=('job', data_kosova_job,))
-thread_telegrafi = threading.Thread(target=process_data, args=('job', data_telegrafi,))
-thread_gjirafa = threading.Thread(target=process_data, args=('job', data_gjirafa,))
-thread_osce = threading.Thread(target=process_data, args=('tender', data_osce,))
-thread_world_bank = threading.Thread(target=process_data, args=('tender', data_world_bank,))
-thread_undp = threading.Thread(target=process_data, args=('tender', data_undp,))
-thread_cdf = threading.Thread(target=process_data, args=('tender', data_cdf,))
-thread_indeed = threading.Thread(target=process_data, args=('job', data_indeed,))
-
-thread_caritas.start()
-thread_kcs.start()
-thread_kastori.start()
-thread_kastori_tender.start()
-thread_kosova_job.start()
-thread_telegrafi.start()
-thread_gjirafa.start()
-thread_osce.start()
-thread_world_bank.start()
-thread_undp.start()
-thread_cdf.start()
-thread_indeed.start()
-
-thread_caritas.join()
-thread_kcs.join()
-thread_kastori.join()
-thread_kastori_tender.join()
-thread_kosova_job.join()
-thread_telegrafi.join()
-thread_gjirafa.join()
-thread_osce.join()
-thread_world_bank.join()
-thread_undp.join()
-thread_cdf.join()
-thread_indeed.join()
+with open("logs.txt", "w") as file:
+    for log in logs:
+        file.write(f"{log[0]} - Response: {log[1]}\n")
