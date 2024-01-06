41c0e20b67886f1f7f280770d73aa95b59a69edd
Ilir <ilir.perolli@tenton.co>
2024-01-06 16:24:02 +0100

    Update ArbeitsAgentur.py

--- jobs/ArbeitsAgentur.py
+++ jobs/ArbeitsAgentur.py
@@ -9,31 +9,27 @@ class ArbeitsAgentur:
 
     def __init__(self):
         self.parsedData = []
-        self.popularJobs = ['Software Developer', 'Customer service representative',
-                            'Data science', 'Designer', 'Financial Analyst', 'Information Security Analyst',
-                            'Database Administrator', 'Statistician', 'Krankenschwester', 'Doctor']
+        self.popularJobs = ['Informatik', 'IT-Systemanalyse, -Anwendungsberatung und -Vertrieb',
+                            'Softwareentwicklung und Programmierung', 'Einkauf und Vertrieb',
+                            'Steuerberatung', 'Unternehmensorganisation und -strategie',
+                            'IT-Netzwerktechnik, -Administration, -Organisation', 'Werbung und Marketing', 'Personalwesen und -dienstleistung',
+                            'Versicherungs- und Finanzdienstleistungen', 'Elektrotechnik', 'Rechtsberatung, -sprechung und -ordnung']
 
     def get_data(self):
 
-        for category in self.popularJobs:
-            url = f"https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/jobs?was={category}&page=1&size=25"
-
-            data = self.fetch_data_from_api(url)
-
-            if data is None:
-                continue
-
-            jobs = data.get('stellenangebote', None)
-            if not jobs:
-                continue
-
-            self.get_jobs(jobs, category)
+        self.get_jobs_based_on_type(is_remote=True)
+        self.get_jobs_based_on_type(is_remote=False)
 
         return self.parsedData
 
-    def get_jobs(self, jobs, category):
+    def get_jobs(self, jobs, category, is_remote):
         for item in jobs:
-            title = item['titel']
+
+            title = item.get('titel', None)
+
+            if not title:
+                continue
+
             url = f"https://www.arbeitsagentur.de/jobsuche/jobdetail/{item['refnr']}"
             provider = "ArbeitsAgentur"
 
@@ -52,7 +48,27 @@ class ArbeitsAgentur:
                     "categories": [category],
                     "city": city,
                     'country': "Germany",
+                    "is_remote": is_remote,
                 })
+
+    def get_jobs_based_on_type(self, is_remote):
+        for category in self.popularJobs:
+
+            if is_remote:
+                url = f"https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/jobs?berufsfeld={category}&page=1&size=25&arbeitszeit=ho"
+            else:
+                url = f"https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/jobs?berufsfeld={category}&page=1&size=25"
+
+            data = self.fetch_data_from_api(url)
+
+            if data is None:
+                continue
+
+            jobs = data.get('stellenangebote', None)
+            if not jobs:
+                continue
+
+            self.get_jobs(jobs, category, is_remote)
 
     def fetch_data_from_api(self, api_url):
         try:
